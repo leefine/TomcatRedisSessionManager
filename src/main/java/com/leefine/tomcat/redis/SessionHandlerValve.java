e9070cdb9d6a3ec92b5634b1377483b516a5ade3
leefine <lihuai.chen@163.com>
2017-11-23 12:54:47 +0800

    Update SessionHandlerValve.java

--- src/main/java/com/leefine/tomcat/redis/SessionHandlerValve.java
+++ src/main/java/com/leefine/tomcat/redis/SessionHandlerValve.java
@@ -9,26 +9,17 @@ import org.apache.catalina.connector.Response;
 import org.apache.catalina.valves.ValveBase;
 
 /**
- * Tomcat clustering with Redis data-cache implementation.
- * 
- * Valve that implements per-request session persistence. It is intended to be
- * used with non-sticky load-balancers.
-
+ * Tomcat clustering with Redis data-cache implementation. * 
+ * Valve that implements per-request session persistence. It is intended to be used with non-sticky load-balancers.
  */
 public class SessionHandlerValve extends ValveBase {
 
 	private SessionManager manager;
 
-	/**
-	 * To set session manager
-	 * 
-	 * @param manager
-	 */
 	public void setSessionManager(SessionManager manager) {
 		this.manager = manager;
 	}
 
-	/** {@inheritDoc} */
 	@Override
 	public void invoke(Request request, Response response) throws IOException, ServletException {
 		try {
