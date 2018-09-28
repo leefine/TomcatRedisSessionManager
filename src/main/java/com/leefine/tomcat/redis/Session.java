945755a332deb2adb61e951c8271fbdba9491405
leefine <lihuai.chen@163.com>
2018-09-28 15:57:09 +0800

    Update Session.java

--- src/main/java/com/leefine/tomcat/redis/Session.java
+++ src/main/java/com/leefine/tomcat/redis/Session.java
@@ -10,8 +10,6 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.security.Principal;
 import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Tomcat clustering with Redis data-cache implementation.
@@ -22,35 +20,8 @@ public class Session extends StandardSession {
 
     private Log log = LogFactory.getLog(SessionManager.class);
 
-    protected Boolean dirty;
-    protected Map<String, Object> changedAttributes;
-    protected static Boolean manualDirtyTrackingSupportEnabled = false;
-    protected static String manualDirtyTrackingAttributeKey = "__changed__";
-
     public Session(Manager manager) {
         super(manager);
-        resetDirtyTracking();
-    }
-
-    public void resetDirtyTracking() {
-        this.changedAttributes = new HashMap<>();
-        this.dirty = false;
-    }
-
-    public static void setManualDirtyTrackingSupportEnabled(boolean enabled) {
-        manualDirtyTrackingSupportEnabled = enabled;
-    }
-
-    public static void setManualDirtyTrackingAttributeKey(String key) {
-        manualDirtyTrackingAttributeKey = key;
-    }
-
-    public Boolean isDirty() {
-        return this.dirty || !this.changedAttributes.isEmpty();
-    }
-
-    public Map<String, Object> getChangedAttributes() {
-        return this.changedAttributes;
     }
 
     /**
@@ -76,23 +47,8 @@ public class Session extends StandardSession {
      */
     @Override
     public void setAttribute(String key, Object value) {
-        if (manualDirtyTrackingSupportEnabled && manualDirtyTrackingAttributeKey.equals(key)) {
-            this.dirty = true;
-            return;
-        }
-
-        Object oldValue = getAttribute(key);
         super.setAttribute(key, value);
-
-        if ((value != null || oldValue != null)
-                && (value == null && oldValue != null || oldValue == null && value != null
-                || !value.getClass().isInstance(oldValue) || !value.equals(oldValue))) {
-            if (this.manager instanceof SessionManager && ((SessionManager) this.manager).getSaveOnChange()) {
-                ((SessionManager) this.manager).save(this, true);
-            } else {
-                this.changedAttributes.put(key, value);
-            }
-        }
+        ((SessionManager) this.manager).save(this);
     }
 
     /**
@@ -108,7 +64,9 @@ public class Session extends StandardSession {
      */
     @Override
     public Enumeration<String> getAttributeNames() {
-        return super.getAttributeNames();
+        if (this.isValid)
+            return super.getAttributeNames();
+        else return null;
     }
 
     /**
@@ -117,11 +75,7 @@ public class Session extends StandardSession {
     @Override
     public void removeAttribute(String name) {
         super.removeAttribute(name);
-        if (this.manager instanceof SessionManager && ((SessionManager) this.manager).getSaveOnChange()) {
-            ((SessionManager) this.manager).save(this, true);
-        } else {
-            this.dirty = true;
-        }
+        ((SessionManager) this.manager).save(this);
     }
 
     /**
@@ -130,7 +84,7 @@ public class Session extends StandardSession {
     @Override
     public void setPrincipal(Principal principal) {
         super.setPrincipal(principal);
-        this.dirty = true;
+        //this.dirty = true;
     }
 
     /**
@@ -146,9 +100,17 @@ public class Session extends StandardSession {
      * {@inheritDoc}
      */
     @Override
-    public void readObjectData(ObjectInputStream in) throws IOException, ClassNotFoundException {
-        super.readObjectData(in);
-        this.setCreationTime(in.readLong());
+    public void readObjectData(ObjectInputStream in) {
+        try {
+            super.readObjectData(in);
+            this.setCreationTime(in.readLong());
+        } catch (IOException e) {
+            log.error(e);
+            log.error(in);
+        } catch (ClassNotFoundException e) {
+            log.error(e);
+            log.error(in);
+        }
     }
 
     /**
